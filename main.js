1c084ed170b0df10aa3a4121bb9709c40a85f521
Giovanni <johnnyontheweb@gmail.com>
2021-03-11 19:00:27 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//~ import { upperCase, localeUpperCase } from "upper-case";
+const { app, BrowserWindow } = require('electron')
+var upcase = require('upper-case');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+    var apc = upcase.upperCase('test');
+  console.log(apc);
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
