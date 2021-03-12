151cd10dc40476ad64f1fba56a6463756a6c1490
Giovanni <johnnyontheweb@gmail.com>
2021-03-12 21:48:15 +0100

    uppercase lib removed

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 //~ import { upperCase, localeUpperCase } from "upper-case";
 const { app, BrowserWindow } = require('electron')
-const upcase = require('upper-case');
-
-var apc = upcase.upperCase('test');
+//~ const upcase = require('upper-case');
+//~ This is a <script>document.write(window.process.argv.slice(-1))</script>.
+//~ var apc = upcase.upperCase('test');
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-    additionalArguments: [apc]
+      contextIsolation: false
+    //~ additionalArguments: [apc]
     }
   })
-  win.toggleDevTools();
-  console.log(apc);
+  //~ win.toggleDevTools();
+  //~ console.log(apc);
   win.loadFile('index.html')
 }
 
